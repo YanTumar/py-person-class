c5c20e48c5567acc1e9ed88347e19d9cf0df0ec1
Yan Tumar <jantumar.unity@gmail.com>
2025-10-27 15:57:28 +0200

    Fixed

--- app/main.py
+++ app/main.py
@@ -8,18 +8,15 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person_list = []
-    for person_dict in people:
-        person = Person(person_dict["name"], person_dict["age"])
-        person_list.append(person)
+    persons = [Person(person_data["name"], person_data["age"])
+               for person_data in people]
+    for person_data in people:
+        person = Person.people[person_data["name"]]
+        wife = person_data.get("wife")
+        husband = person_data.get("husband")
 
-    for i, person_dict in enumerate(people):
-        if "wife" in person_dict and person_dict["wife"] is not None:
-            wife_name = person_dict["wife"]
-            person_list[i].wife = Person.people[wife_name]
-
-        if "husband" in person_dict and person_dict["husband"] is not None:
-            husband_name = person_dict["husband"]
-            person_list[i].husband = Person.people[husband_name]
-
-    return person_list
+        if wife:
+            person.wife = Person.people[wife]
+        if husband:
+            person.husband = Person.people[husband]
+    return persons
